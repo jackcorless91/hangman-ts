dc8acc52eeb2553195e6a8232cc7be2bb7303a02
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-08-10 20:22:20 +1000

    drawing component created, app.tsc configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import {useState} from "react";
+import { useState} from "react";
+import words from "./wordList.json"
+import {HangmanDrawing} from "./HangmanDrawing.tsx";
+import {HangmanWord} from "./HangmanWord.tsx";
+import {Keyboard} from "./keyboard.tsx";
 
 function App() {
-    const [wordToGuess, setWordToGuess] = useState("Test")
-    return <h1>hi</h1>
+    const [wordToGuess, setWordToGuess] = useState(() => {
+        return words[Math.floor(Math.random() * words.length)]
+    })
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+
+    return (
+        <div
+        style={{
+            maxWidth: "800px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+            margin: "0 auto",
+            alignItems: "center"
+        }}>
+        <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+            <HangmanDrawing />
+            <HangmanWord />
+            <Keyboard />
+        </div>
+    )
 }
 
 export default App
