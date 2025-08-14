151e56c0cf34cd15d57c0ab33fcd964609b2c1c4
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-08-14 13:44:20 +1000

    keyboard on screen map

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,10 @@ function App() {
         <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
             <HangmanDrawing />
             <HangmanWord />
-            <Keyboard />
+            <div style={{ alignSelf: "stretch",  }}>
+                <Keyboard />
+            </div>
+
         </div>
     )
 }
